afd7a8b1075d3f09bcf6be83e851b25519f9215b
Daniel Bradburn <moagstar@gmail.com>
2015-07-25 20:50:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,37 @@
-from distutils.core import setup
+from os import path
+from setuptools import setup
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(path.join(*paths), 'r') as f:
+        return f.read()
+
 setup(
-  name = 'table2dicts',
-  packages = ['table2dicts'],
-  version = '0.1a',
-  description = 'Python module for converting a html table to a list of dictionaries.',
-  author = 'Daniel Bradburn',
-  author_email = 'moagstar@gmail.com',
-  url = 'https://github.com/moagstar/table2dicts',
-  download_url = 'https://github.com/moagstar/table2dicts/tarball/0.1a',
-  keywords = ['html', 'scrape',],
-  classifiers = [],
+    name='table2dicts',
+    version='0.1b',
+    description='Parse and split PEM files painlessly.',
+    long_description=(read('README.rst')),
+    url='http://github.com/moagstar/table2dicts/',
+    download_url = 'https://github.com/moagstar/table2dicts/tarball/0.1b',
+    license='MIT',
+    author='Daniel Bradburn',
+    author_email='moagstar@gmail.com',
+    py_modules=['table2dicts'],
+    include_package_data=True,
+    install_requires = ['BeautifulSoup4'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
