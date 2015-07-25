e1153b2f6f1659fcf6193d8168a8a3e91b2408d1
Daniel Bradburn <moagstar@gmail.com>
2015-07-25 18:58:23 +0200

    move to subfolder

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
-__author__ = 'moagstar'
+from distutils.core import setup
+setup(
+  name = 'table2dicts',
+  packages = ['table2dicts'],
+  version = '0.1',
+  description = 'Python module for converting a html table to a list of dictionaries.',
+  author = 'Daniel Bradburn',
+  author_email = 'moagstar@gmail.com',
+  url = 'https://github.com/moagstar/table2dicts',
+  download_url = 'https://github.com/moagstar/table2dicts/archive/master.zip',
+  keywords = ['html', 'scrape',],
+  classifiers = [],
+)
