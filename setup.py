ff65e1f5ce10546b32e1b08325cf10ed0ee42b35
Daniel Bradburn <moagstar@gmail.com>
2015-07-25 20:39:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'table2dicts',
   packages = ['table2dicts'],
-  version = '0.1',
+  version = '0.1a',
   description = 'Python module for converting a html table to a list of dictionaries.',
   author = 'Daniel Bradburn',
   author_email = 'moagstar@gmail.com',
